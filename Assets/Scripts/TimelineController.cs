cb321302d0304bfdbc9840d5464d863290c59dee
runtime <erik.kroha@gmail.com>
2018-07-22 21:07:39 -0400

    first commit

---
+++ Assets/Scripts/TimelineController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class TimelineController : MonoBehaviour
+{
+
+    public PlayableDirector playableDirector;
+    private string _currentState;
+
+
+    public void swing()
+    {
+        _currentState = "swing";
+        Debug.Log(_currentState);
+        playableDirector.initialTime = 2;
+        playableDirector.Play();
+    }
+
+    public void idle() {
+        _currentState = "idle";
+        Debug.Log(_currentState);
+        playableDirector.initialTime = 0;
+        playableDirector.Play();
+
+    }
+
+    void Update()
+    {
+       
+        if (_currentState==null) {
+            idle(); 
+        }
+
+        if (Input.GetKeyDown("w"))
+        {
+            if(_currentState== "idle") {
+                playableDirector.Stop();
+                swing();
+            }
+            else
+            {
+                playableDirector.Stop();
+                swing();
+            }
+                   
+        }
+
+        if ((_currentState=="swing")&&(playableDirector.time > 3)) {
+            playableDirector.Stop();
+            idle();
+        }
+
+        if ((_currentState == "idle") && (playableDirector.time > 1)) {
+            playableDirector.Stop();
+            idle();
+        }
+
+    }
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # TimelineControllerStateMachine
 a simple state machine for the unity timeline
+
+
+Simple State Machine using Timeline Controller
+
+Lumberjack uses traditional animator and the different states will loop through unless you disable it.
+this seems to be overwritten when timeline is used
+
+
+in TimelineController script we use the playableDirector to control the timeline. The timeline is mapped to the lumberjack.
+The timeline is made up of two of the lumberjacks animations.
+
+if there is no _currentState the animation plays the idle state. the idle state is set to loop.
+
+state is changed to swing when the w key is pressed.
+
+after the swing, the character returns to the idle state.
+
